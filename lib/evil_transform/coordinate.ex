e9990fd2ce4474c4e06d88072bb87aca4af1b00f
Yin Weijun <gw9812@gmail.com>
2017-07-31 13:47:19 +0800

    add Coordinate module docs

--- lib/evil_transform/coordinate.ex
+++ lib/evil_transform/coordinate.ex
@@ -1,5 +1,10 @@
 defmodule EvilTransform.Coordinate do
-  
+  @moduledoc """
+  This module defines `EvilTransform.Coordinate` struct, which contains
+  latitude and longitude.
+
+  """
+
   defstruct(
     lat: 0.0,
     lng: 0.0
