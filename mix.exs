0bab669b16a39cfdc9c4af9365cf4a600739e1bf
Yin Weijun <gw9812@gmail.com>
2017-07-31 12:25:58 +0800

    add dependencies

--- mix.exs
+++ mix.exs
@@ -7,22 +7,28 @@ defmodule EvilTransform.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  def package() do
+    [
+      maintainers: [" Yin Weijun "],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/weij/eviltransform"}
+    ]  
+  end
+
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.16.2"},
     ]
   end
 end
